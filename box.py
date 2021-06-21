aecbb886521a98a18d6fafec3f03932a3a27eae8
wangt3 <twang3@colgate.edu>
2021-06-21 08:48:48 -0700

    Box fixes

--- box.py
+++ box.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod, ABCMeta
+import heapdict
 #import test
 import input
 
@@ -41,23 +42,33 @@ class PriceBox(AbstractBox):
         self.__financials = financials
         self._resultList = [[]]
 
-    def tickerEvaluator(self, dtTicker):  # needs the 2 dates that are to be analyzed, also needs the ticker or tickers needed. If it only works with one ticker might want to do it inside evaluator
+    def tickerEvaluator(self, ticker):  # needs the 2 dates that are to be analyzed, also needs the ticker or tickers needed. If it only works with one ticker might want to do it inside evaluator
         
-        ticker = self.__dt(dtTicker) #pulls out the company value as pandas 
+        df = input.get_stock_price_data_withPD(self.__financials,ticker,self.__startDate,self.__endDate,'daily')
+        print (df)
+        return 
+ 
+         #pulls out the company value as pandas 
         # get value of ticker at startDate
-        # get value of ticker at endDate-1
+        # get value of ticker at endDate-1110
         # compute percent price increase by startDate value/ endDate-1 value
-        # 
-
+    
     def totalEvaluator(self, tickerList):
+        #
+        hd = dict.fromkeys(tickerList)
+
+        for ticker in tickerList:
+            tickerValue = self.tickerEvaluator(ticker)
+            hd[ticker]= tickerValue
+        return hd
+        
         # get the dictionary 
         # run ticketEvaluator for all tickers in tickerList \/\/\/
         # for i in tickerList:
         #   binary_tree.append(tickerEvaluator(self, i))
         # sort the dictionary with some sort of 21a heresy to rank every company
         # return that 
-        return 
-
+        
     def equalizer(self):
         pass #pass for now, there is only one box 
     
