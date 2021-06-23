385f2c615cf951b6e6076c789d4ef2ca57f6d2d0
wangt3 <twang3@colgate.edu>
2021-06-23 10:59:12 -0700

    WACC calculations appear to be working todo list within function calcWACC for more details

--- box.py
+++ box.py
@@ -33,7 +33,8 @@ class AbstractBox(ABC):
     @abstractmethod
     def equalizer(self):#this balances the results so diff boxes can be compared
         pass
- 
+
+
 class PriceBox(AbstractBox):
     def __init__(self, startDate, endDate, financials):
          #assumed dictionary of stocks we need to pull
@@ -72,9 +73,113 @@ class PriceBox(AbstractBox):
     def equalizer(self):
         pass #pass for now, there is only one box 
     
-    if __name__ == "__main__":
-        # send to output
-        #stocks for testing: GME, AMC, TSLA
 
-        pass 
+
+class DCFbox(AbstractBox):
+    def __init__(self,financials):
+        self.__financials = financials
+        #WACC stuff here
+    def tickerEvaluator(self):
+        pass
+
+    def totalEvaluator(self):
+        pass
+
+    def equalizer(self):
+        pass
+
+    def calcWACC(self, FVXdata, GSPCdata, tickerdataIncome, tickerdataBalance, tickerdataCash, tickerdataSummary):
+        #variables needed: 
+        # WACC needs Cost of Equity and Cost of Debt 
+
+        #Risk Free Rate --> 
+        #Company Beta
+        #Market Risk Premium = Market Return - Risk Free Rate
+        #Cost of Equity = Risk Free Rate + (Company Beta * Market Risk Premium)
+
+        #Interest Expense
+        #Total Debt
+        #Cost of Debt = Interest Expense/Total Debt
+    
+        #WACC
+        # Enterprise Value = Equity Value + Total Debt - Cash
+        # Tax Rate = Tax Expense/EBIT
+        # Equity Weight = Equity Value/Enterprise Value
+        # Equity Value = Current Share Price*Shares Outstanding
+        # Debt Weight = Total Debt/Enterprise Value
+        # WACC = (Equity Weight * Cost of Equity) + (Debt Weight * Cost of Debt * (1 - Tax Rate))
+
+        # riskFreeRate = constant, use yahoo finance [^FVX]
+        # beta = beta # grab this with test = yf.get_summary_data(financials,reformat=True)
+        # marketReturn = e #percent change of ['^GSPC'] ticker (S&P 500 with 1 year) of yf.get_historical_price_data(financials,start_date,end_date,'monthly')
+        # interestExpense = get_financial_stmts(annual,'income')
+
+        # Possible workarounds: use pandas to compile list of dicts for marketValue values for easier reading
+        
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #TODO
+        #1.) make sure all strings are set as variables eventually (e.g TSLA can and will change)
+        #2.) increase readability, probably though some pandas operation we can ask prabu
+        #3.) abs value everything?
+        #4.) figure out what makes this update every second
+        #5.) accuracy improvement 
+        #5a.) marketReturn is a default number, see if this can be edited. 
+
+
+        # Cost of Equity 
+        riskFreeRate = (FVXdata.get('^FVX').get('prices')[len(FVXdata.get('^FVX').get('prices'))-1].get('close'))/100
+        print ("riskFreeRate: "+str(riskFreeRate))
+        beta = tickerdataSummary.get('TSLA').get('beta') #yf get_beta()
+        print ("beta: "+str(beta))
+
+        #marketReturn calc
+        currentMarketValue = GSPCdata.get('^GSPC').get('regularMarketOpen')
+        previousMarketValue = GSPCdata.get('^GSPC').get('regularMarketOpen') #these two may require a bit of sorting
+        marketReturn = .098 #((currentMarketValue-previousMarketValue)/abs(previousMarketValue))
+        print ("marketReturn: "+str(marketReturn))
+    
+        marketRiskPremium = marketReturn - riskFreeRate
+        print ("marketRiskPremium: "+str(marketRiskPremium))
+
+        costOfEquity = riskFreeRate + (beta*marketRiskPremium)
+        print ("costOfEquity: "+str(costOfEquity))
+        
+        tickerDateIncome = str(list(tickerdataIncome.get('incomeStatementHistory').get('TSLA')[0].keys())[0]) #
+        tickerDateBalance = str(list(tickerdataBalance.get('balanceSheetHistory').get('TSLA')[0].keys())[0]) 
+
+        #Cost of Debt
+        interestExpense = abs(tickerdataIncome.get('incomeStatementHistory').get('TSLA')[0].get(tickerDateIncome).get('interestExpense')) #COMPLETED tickerdata
+        print ("interestExpense: "+str(interestExpense))
+
+        longTermDebt = tickerdataBalance.get('balanceSheetHistory').get('TSLA')[0].get(tickerDateBalance).get('longTermDebt') #COMPLETE
+        shortLongTermDebt = tickerdataBalance.get('balanceSheetHistory').get('TSLA')[0].get(tickerDateBalance).get('shortLongTermDebt') #COMPLETE
+        totalDebt = shortLongTermDebt + longTermDebt
+        costofDebt = interestExpense/totalDebt
+
+        print ("totalDebt: "+str(totalDebt))
+        print ("costofDebt: "+str(+costofDebt))
+
+        #WACC calc
+        ebit = tickerdataIncome.get('incomeStatementHistory').get('TSLA')[0].get(tickerDateIncome).get('ebit') #COMPLETE
+        print ("ebit: "+str(ebit))
+
+        equityValue = tickerdataSummary.get('TSLA').get("marketCap")
+        cash = tickerdataBalance.get('balanceSheetHistory').get('TSLA')[0].get(tickerDateBalance).get('cash') #COMPLETE
+        enterpriseValue = equityValue+totalDebt-cash
+        equityWeight = equityValue/enterpriseValue
+        debtWeight = totalDebt/enterpriseValue
+
+        taxExpense = tickerdataIncome.get('incomeStatementHistory').get('TSLA')[0].get(tickerDateIncome).get('incomeTaxExpense') #COMPLETE 
+        
+        taxRate = taxExpense/ebit
+        WACC = (equityWeight*costOfEquity)+(debtWeight*costofDebt*(1-taxRate))
+        print ("WACC: "+str(WACC))
+        return WACC
+
+    def calcFCFF(self): 
+        pass
+if __name__ == "__main__":
+    # send to output
+    #stocks for testing: GME, AMC, TSLA
+    pass 
 
